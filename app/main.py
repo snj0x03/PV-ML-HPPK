28776c3f1f3cad2fe2b0ee2c8ad23c0aa0a6c16c
snj0x03 <sawnaysoe@outlook.com>
2026-06-17 13:49:45 +0900

    modified:   app/main.py 	modified:   app/static/index.html 	renamed:    models/best.pt -> models/best_det.pt 	new file:   models/best_v26_cls.pt 	new file:   models/best_v26_det.pt

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ from PIL import Image
 
 from ultralytics import YOLO
 from database import (
-        SessionLocal, DetectionLog, Part,
+        SessionLocal, DetectionLog, ClassificationLog, Part,
     init_db, clear_detection_history,
 )
 
@@ -21,9 +21,11 @@ STATIC_DIR = os.path.join(BASE_DIR, "static")
 app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 
 PROJECT_ROOT = os.path.dirname(BASE_DIR)
-MODEL_PATH = os.path.join(PROJECT_ROOT, "models", "best_v11_resume.pt")
+MODEL_DET_PATH = os.path.join(PROJECT_ROOT, "models", "best_v26_det.pt")
+MODEL_CLS_PATH = os.path.join(PROJECT_ROOT, "models", "best_v26_cls.pt")
 
-model = YOLO(MODEL_PATH)
+model_det = YOLO(MODEL_DET_PATH)
+model_cls = YOLO(MODEL_CLS_PATH)
 
 IMAGES_DIR = os.path.join(BASE_DIR, "data", "detection_images")
 os.makedirs(IMAGES_DIR, exist_ok=True)
@@ -58,7 +60,7 @@ async def main_page():
 
 
 @app.post("/detect")
-async def predict_part(file: UploadFile = File(...), db: Session = Depends(get_db)):
+async def detect(file: UploadFile = File(...), db: Session = Depends(get_db)):
     print(f"[Predict] request: {file.filename}")
 
     try:
@@ -69,7 +71,7 @@ async def predict_part(file: UploadFile = File(...), db: Session = Depends(get_d
         image.save(buffered, format="JPEG")
         img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
 
-        results = model(image, conf=0.25)
+        results = model_det(image, conf=0.25)
 
         plotted_bgr = results[0].plot()
         plotted_rgb = Image.fromarray(plotted_bgr[..., ::-1])
@@ -79,26 +81,30 @@ async def predict_part(file: UploadFile = File(...), db: Session = Depends(get_d
 
         saved_path = save_result_image(Image.fromarray(plotted_bgr[..., ::-1]), file.filename)
 
-        res = dict()
+        bboxes = [] 
+        conf = []
+        classes = []
         for result in results:
-            res["bboxes"] = result.boxes.xywhn.cpu().numpy().tolist()
-            res["confidences"] = result.boxes.conf.cpu().numpy().tolist()
-            res["classes"] = result.boxes.cls.cpu().numpy().tolist()
+            bboxes = result.boxes.xywhn.cpu().numpy().tolist()
+            conf = result.boxes.conf.cpu().numpy().tolist()
+            classes = result.boxes.cls.cpu().numpy().tolist()
+
+        n = len(classes)
         srl = []
         msg = []
         send = []
-        for i in range(len(res["classes"])):
-            qry = db.query(Part).filter(Part.class_id == res["classes"][i]).first()
+
+        # Database Commit & Message
+        for i in range(n):
+            qry = db.query(Part).filter(Part.class_id == classes[i]).first()
             srl.append(qry.serial_number)
             msg.append(qry.part_desc)
             send.append({
                 "class": qry.serial_number,
-                "confidence": res["confidences"][i],
-                "bbox": res["bboxes"][i]
+                "confidence": conf[i],
+                "bbox": bboxes[i]
             })
                         
-            
-
         log = DetectionLog(
             image_url=saved_path,
             result=send
@@ -107,13 +113,68 @@ async def predict_part(file: UploadFile = File(...), db: Session = Depends(get_d
         db.commit()
         db.refresh(log)
 
-        return {"image": img_str, "conf": res["confidences"], "cls": srl, "message": msg}
+        return {"image": img_str, "conf": conf, "cls": srl, "message": msg}
 
     except Exception as e:
         print(f"[predict] error: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Processing error: {str(e)}")
 
+@app.post("/classify")
+async def classify(file: UploadFile = File(...), db: Session = Depends(get_db)):
+    print(f"[Predict] request: {file.filename}")
 
+    try:
+        image_bytes = await file.read()
+        image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+
+        buffered = io.BytesIO()
+        image.save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+        results = model_cls(image, conf=0.25)
+
+        plotted_bgr = results[0].plot()
+        plotted_rgb = Image.fromarray(plotted_bgr[..., ::-1])
+        buf = io.BytesIO()
+        plotted_rgb.save(buf, format="JPEG")
+        img_str = base64.b64encode(buf.getvalue()).decode("utf-8")
+
+        saved_path = save_result_image(Image.fromarray(plotted_bgr[..., ::-1]), file.filename)
+
+        conf = []
+        classes = []
+        for result in results:
+            probs = result.probs
+            conf = probs.top5conf.cpu().numpy().tolist()
+            classes = [result.names[i] for i in probs.top5]
+
+
+        n = len(classes)
+        msg = []
+        send = []
+
+        # Database Commit & Message
+        for i in range(n):
+            qry = db.query(Part).filter(Part.serial_number == classes[i]).first()
+            msg.append(qry.part_desc)
+            send.append({
+                "class": classes[i],
+                "confidence": conf[i],
+            })
+                        
+        log = ClassificationLog(
+            image_url=saved_path,
+            result=send
+        )
+        db.add(log)
+        db.commit()
+        db.refresh(log)
+
+        return {"image": img_str, "conf": conf, "cls": classes, "message": msg}
+
+    except Exception as e:
+        print(f"[predict] error: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Processing error: {str(e)}")
 
 @app.delete("/admin/clear-db")
 async def clear_db(db: Session = Depends(get_db)):
